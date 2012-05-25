0937c9a952d6d464b815afe998cd8565ca99642d
Emerson Estrella <emerson.estrella@gmail.com>
2012-05-25 02:40:59 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locomotive::Application.config.secret_token = '0392b61f6cafb81bed552b2593fed26bda4745c29e624230a7c046ce552edea133ffc4041ed5b9054237be29558b07b6d7467f7b6b40382508aea0de61b7e5ac'
